45d20250c4d1eafd0cc66e05569a8a5c35d3343b
Brian T. Wightman <MidLifeXis@wightmanfam.org>
2013-03-17 15:09:35 -0500

    Add documentation for Algorithm::CheckDigits

--- lib/Algorithm/Damm.pm
+++ lib/Algorithm/Damm.pm
@@ -129,6 +129,13 @@ __END__
 
 Original code based on L<Algorithm::LUHN> by Tim Ayers.
 
+=item Algorithm::CheckDigits
+
+L<Algorithm::CheckDigits> is a module that this probably should have
+been written as a part of.  There is an open issue
+(L<https://github.com/MidLifeXis/perl-algorithm-damm/issues/1>) to add
+an integration layer for this module to L<Algorithm::CheckDigits>.
+
 =item Wikipedia
 
 L<http://en.wikipedia.org/wiki/Damm_algorithm> - Article explaining
